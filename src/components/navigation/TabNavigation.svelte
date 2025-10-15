f910266b0e51f29adc4b71b624cfa0f1cd00fa56
trongnguyen24 <6469345+trongnguyen24@users.noreply.github.com>
2025-10-15 23:41:21 +0700

    Update TabNavigation loading indicator style
    Added 'w-full' to the loading indicator container in TabNavigation.svelte for improved layout. Also bumped package version to 2.2.8.

--- src/components/navigation/TabNavigation.svelte
+++ src/components/navigation/TabNavigation.svelte
@@ -100,7 +100,7 @@
           </svg>
         {/if}
         <div
-          class="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 {tab.isLoading
+          class="flex w-full items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 {tab.isLoading
             ? 'opacity-0'
             : ''}"
         >

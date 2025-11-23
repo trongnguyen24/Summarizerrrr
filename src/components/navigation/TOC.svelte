94e64d5bcd251f9aa1b914ee7c03d76cc1076f13
trongnguyen24 <trongnguyen24@gmail.com>
2025-11-23 13:10:41 +0700

    Update TOC.svelte

--- src/components/navigation/TOC.svelte
+++ src/components/navigation/TOC.svelte
@@ -214,7 +214,7 @@
     <div class="relative">
       <div
         id="toc-scroll"
-        class="w-64 xs:w-80 overflow-auto max-h-[calc(100vh-150px)] border rounded-t-lg border-border bg-surface-1"
+        class="w-[clamp(280px,70vw,400px)] overflow-auto max-h-[calc(100vh-150px)] border rounded-t-lg border-border bg-surface-1"
       >
         <div
           class="flex flex-col divide-y divide-border/50 dark:divide-border/70"
